92ac6386cadaf9a0950955d90dc21338d346d66d
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2022-12-08 14:14:26 +0100

    added else statement

--- main.dart
+++ main.dart
@@ -2,10 +2,12 @@ void main() {
   // int age = 18;
   // bool isAdult = age >= 18;
   // print(isAdult);
-  int age = 18;
+  int age = 10;
   bool isAdult = age >= 18;
   if (age >= 18) {
     // write code here
     print("go to main church");
+  } else {
+    print("go home");
   }
 }
