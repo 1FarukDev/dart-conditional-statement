a055718436a9f6d82a8554c91ec5f365c08a0a0c
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2022-12-08 13:44:28 +0100

    first if statement

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main() {
+  // int age = 18;
+  // bool isAdult = age >= 18;
+  // print(isAdult);
+  int age = 18;
+  bool isAdult = age >= 18;
+  if (age >= 18) {
+    // write code here
+    print("go to main church");
+  }
+}
